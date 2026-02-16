bf40ee9d281abdf3a019d747ea2266d8da35bb33
Dominik Kowalski <dominikkowalski331@gmail.com>
2026-02-16 16:51:11 +0100

    Create final main grid design

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,137 @@
-import Image from "next/image";
+"use client";
+
+import { motion, easeOut } from "framer-motion";
+import Link from "next/link";
+
+const containerVariants = {
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren: 0.15,
+    },
+  },
+};
+
+const cardVariants = {
+  hidden: { opacity: 0, y: 20 },
+  show: { opacity: 1, y: 0, transition: { duration: 0.6, ease: easeOut } },
+};
 
 export default function Home() {
   return (
-    // Come back to this, this dummy grid is some straight bs
-    <main className="flex flex-wrap items-start pt-28 px-6 gap-2 bg-(--color-bg) min-h-2 max-w-5xl mx-auto">
-      
-      <div className="w-4xl min-h-56 border border-blue-500 rounded-xl shadow-md p-6 bg-black text-white hover:shadow-lg transition-shadow">
+    <motion.main
+      className="grid grid-cols-1 sm:grid-cols-4 gap-2 grid-flow-dense bg-(--color-bg) min-h-2 sm:max-w-5xl m-4 sm:mx-auto mt-24"
+      initial="hidden"
+      animate="show"
+      variants={containerVariants}
+    >
+      <Link 
+      href="/followed" 
+      className="col-span-4 min-h-56 border border-blue-500 rounded-xl shadow-md p-6 bg-linear-to-br from-[#0e111a] to-[#1a1f2a] text-white hover:shadow-lg transition-shadow"
+>
+      <motion.div
+      variants={cardVariants}
+      >
         <h2 className="text-2xl font-bold mb-4 text-blue-500">Followed Stocks</h2>
         <ul className="space-y-2">
-          <li className="text-gray-500">Follow stocks to get live updates and notifications </li>
+          <li className="text-gray-500">
+            Follow stocks to get live updates and notifications
+          </li>
         </ul>
-      </div>
+      </motion.div>
+      </Link>
 
-      <div className="w-xl max-w-3xl min-h-56 border border-blue-500 rounded-xl shadow-md p-6 bg-black text-white hover:shadow-lg transition-shadow">
-        <h2 className="text-2xl font-bold mb-4 text-blue-500">Trending News</h2>
-        <ul className="space-y-4 text-xl">
-          <li>Market rallies as tech stocks climb <span className="text-gray-500 text-xs ml-2">thedailywire.com</span></li>
-          <li>Federal Reserve announces new policy <span className="text-gray-500 text-xs ml-2">thedailywire.com</span></li>
-          <li>Elon Musk teases new Tesla product <span className="text-gray-500 text-xs ml-2">thedailywire.com</span></li>
-        </ul>
-      </div>
+      <Link
+        href="/news"
+        className="block col-span-3 min-h-56 border border-blue-500 rounded-xl shadow-md p-6 
+                  bg-linear-to-br from-[#0e111a] to-[#1a1f2a] text-white hover:shadow-lg 
+                  transition-shadow cursor-pointer"
+      >
+        <motion.div variants={cardVariants}>
+          <div>
+            <h2 className="text-2xl font-bold mb-4 text-blue-500">Trending News</h2>
+            <ul className="space-y-4 text-xl">
+              <li>
+                Market rallies as tech stocks climb{" "}
+                <span className="text-gray-500 text-xs ml-2">thedailywire.com</span>
+              </li>
+              <li>
+                Federal Reserve announces new policy{" "}
+                <span className="text-gray-500 text-xs ml-2">thedailywire.com</span>
+              </li>
+              <li>
+                Elon Musk teases new Tesla product{" "}
+                <span className="text-gray-500 text-xs ml-2">thedailywire.com</span>
+              </li>
+            </ul>
+          </div>
+        </motion.div>
+      </Link>
 
-      <div className="w-xs max-w-3xl min-h-56 border border-blue-500 rounded-xl shadow-md p-6 bg-black text-white hover:shadow-lg transition-shadow">
-        <h2 className="text-2xl font-bold mb-4 text-blue-500">Market Summary</h2>
-        <ul className="space-y-4 text-xl">
-          <li>S&P 500: 4250 <span className="text-xs text-red-500">(-6.7%)</span></li>
-          <li>DOW  10000 <span className="text-xs text-red-500">(-6.7%)</span></li>
-          <li>NR3 6969 <span className="text-xs text-red-500">(-6.7%)</span></li>
-        </ul>
-      </div>
+      <Link
+        href="/summary"
+        className="block col-span-1 min-h-56 border border-blue-500 rounded-xl shadow-md p-6 
+                  bg-linear-to-br from-[#0e111a] to-[#1a1f2a] text-white hover:shadow-lg 
+                  transition-shadow cursor-pointer"
+      >
+        <motion.div variants={cardVariants}>
+          <div>
+            <h2 className="text-2xl font-bold mb-4 text-blue-500">Market Summary</h2>
+            <ul className="space-y-4 text-xl">
+              <li>
+                S&amp;P 500: 4250 <span className="text-xs text-red-500">(-6.7%)</span>
+              </li>
+              <li>
+                DOW 10000 <span className="text-xs text-red-500">(-6.7%)</span>
+              </li>
+              <li>
+                NASDAQ 6969 <span className="text-xs text-red-500">(-6.7%)</span>
+              </li>
+            </ul>
+          </div>
+        </motion.div>
+      </Link>
 
-            <div className="w-5/12 max-w-3xl min-h-56 border border-blue-500 rounded-xl shadow-md p-6 bg-black text-white hover:shadow-lg transition-shadow">
+      <Link 
+      href="/stocks/losers"
+      className="col-span-2 border border-blue-500 rounded-xl shadow-md p-6 bg-linear-to-br from-[#0e111a] to-[#1a1f2a] text-white hover:shadow-lg transition-shadow">
+      <motion.div
+        variants={cardVariants}
+      >
         <h2 className="text-2xl font-bold mb-4 text-blue-500">Top Losers Today</h2>
-        <ul className="space-y-2 text-xl">
-          <li>NVIDIA (NVDA) $100.69  <span className="text-xs text-green-500">(+3.4%)</span></li>
-          <li>NVIDIA (NVDA) $100.69  <span className="text-xs text-green-500">(+3.4%)</span></li>
-          <li>NVIDIA (NVDA) $100.69  <span className="text-xs text-green-500">(+3.4%)</span></li>
+        <ul className="space-y-4 text-xl">
+          <li>
+            NVIDIA (NVDA) $100.69 <span className="text-xs text-green-500">(+3.4%)</span>
+          </li>
+          <li>
+            NVIDIA (NVDA) $100.69 <span className="text-xs text-green-500">(+3.4%)</span>
+          </li>
+          <li>
+            NVIDIA (NVDA) $100.69 <span className="text-xs text-green-500">(+3.4%)</span>
+          </li>
         </ul>
-      </div>
+      </motion.div>
+      </Link>
 
-            <div className="w-6/12 max-w-3xl min-h-56 border border-blue-500 rounded-xl shadow-md p-6 bg-black text-white hover:shadow-lg transition-shadow">
+      <Link href="stocks/gainers" className="col-span-2 min-h-56 border border-blue-500 rounded-xl shadow-md p-6 bg-linear-to-br from-[#0e111a] to-[#1a1f2a] text-white hover:shadow-lg transition-shadow"
+>
+      <motion.div
+      variants={cardVariants}
+      >
         <h2 className="text-2xl font-bold mb-4 text-blue-500">Top Gainers Today</h2>
-        <ul className="space-y-2 text-xl">
-          <li>NVIDIA (NVDA) $100.69  <span className="text-xs text-green-500">(+3.4%)</span></li>
-          <li>NVIDIA (NVDA) $100.69  <span className="text-xs text-green-500">(+3.4%)</span></li>
-          <li>NVIDIA (NVDA) $100.69  <span className="text-xs text-green-500">(+3.4%)</span></li>
+        <ul className="space-y-4 text-xl">
+          <li>
+            NVIDIA (NVDA) $100.69 <span className="text-xs text-green-500">(+3.4%)</span>
+          </li>
+          <li>
+            NVIDIA (NVDA) $100.69 <span className="text-xs text-green-500">(+3.4%)</span>
+          </li>
+          <li>
+            NVIDIA (NVDA) $100.69 <span className="text-xs text-green-500">(+3.4%)</span>
+          </li>
         </ul>
-      </div>
-    </main>
+      </motion.div>
+      </Link>
+    </motion.main>
   );
 }
